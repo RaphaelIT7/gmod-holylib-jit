26eb499370430cb4ac654737c5ca85c6bd146429
RaphaelIT7 <64648134+RaphaelIT7@users.noreply.github.com>
2026-03-26 23:14:12 +0100

    Save and restore XMCSR flags when entering & leaving the VM

--- src/lj_obj.h
+++ src/lj_obj.h
@@ -704,6 +704,7 @@ typedef struct global_State {
   MRef ctype_state;	/* Pointer to C type state. */
   PRNGState prng;	/* Global PRNG state. */
   GCRef gcroot[GCROOT_MAX];  /* GC roots. */
+  uint32_t mxcsr;
 } global_State;
 
 #define mainthread(g)	(&gcref(g->mainthref)->th)
