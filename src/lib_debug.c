83bb5be0c0b56a3f9b447e621461803753351749
RaphaelIT7 <64648134+RaphaelIT7@users.noreply.github.com>
2026-04-13 14:09:34 +0200

    Make debug.setfenv & debug.getfenv support nil userdata env
    Now debug.setfenv supports setting nil for userdata only.
    debug.getfenv now properly handles nil env and won't crash anymore.
    Fixed a GC crash when the env on userdata was null

--- src/lib_debug.c
+++ src/lib_debug.c
@@ -58,7 +58,10 @@ LJLIB_CF(debug_getfenv)
 
 LJLIB_CF(debug_setfenv)
 {
-  lj_lib_checktab(L, 2);
+  lj_lib_checktabornil(L, 2);
+  if (!tvisudata(L->base) && tvisnil(L->base+1))
+    lj_err_argt(L, 2, LUA_TTABLE);
+
   L->top = L->base+2;
   if (!lua_setfenv(L, 1))
     lj_err_caller(L, LJ_ERR_SETFENV);
